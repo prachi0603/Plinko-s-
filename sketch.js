1f79686c6806007ccd58405b49d996b7f98db2a5
prachi sharma <67380116+prachi0603@users.noreply.github.com>
2021-08-11 11:51:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -64,7 +64,7 @@ function draw() {
 
   //create the particles using frameCount
 if(frameCount%60===0){
-  particles.push(new Particle(random(width/2-30,width/2+30)))
+  particles.push(new Particle(random(width/2-30,width/2+30),10,10))
 }
 
   //display the particles 
